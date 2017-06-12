0f0cbaa79acbd30a87119316f76440551ceb1ba4
timdiels <timdiels.m@gmail.com>
2017-06-12 17:03:55 +0200

    Refactor: rm trailing whitespace

--- cookiecutters/simple/{{cookiecutter.pypi_name}}/README.rst
+++ cookiecutters/simple/{{cookiecutter.pypi_name}}/README.rst
@@ -5,4 +5,3 @@ Links
 - `Documentation <http://{{cookiecutter.pypi_name|replace("_", "-")}}.readthedocs.io/en/latest/>`_
 - `PyPI <https://pypi.python.org/pypi/{{cookiecutter.pypi_name}}>`_
 - `GitHub <{{cookiecutter.repo_url}}>`_
-
--- cookiecutters/simple/{{cookiecutter.pypi_name}}/{{cookiecutter.pypi_name}}/tests/__init__.py
+++ cookiecutters/simple/{{cookiecutter.pypi_name}}/{{cookiecutter.pypi_name}}/tests/__init__.py
@@ -1,17 +1,17 @@
 # Copyright (C) {% now "utc", "%Y" %} {{cookiecutter.author}} <{{cookiecutter.author_email}}>
-# 
+#
 # This file is part of {{cookiecutter.name}}.
-# 
+#
 # {{cookiecutter.name}} is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # {{cookiecutter.name}} is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public License
 # along with {{cookiecutter.name}}.  If not, see <http://www.gnu.org/licenses/>.
 
